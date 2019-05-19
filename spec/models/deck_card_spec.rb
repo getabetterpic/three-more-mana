c259c9a26f2f640869e92500a511a0784f3b8e93
Dan Smith <dan@getabetterpic.com>
2019-05-19 17:45:43 -0400

    Update deck card spec

--- spec/models/deck_card_spec.rb
+++ spec/models/deck_card_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe DeckCard, type: :model do
 
   context "card uniqueness" do
     subject { FactoryBot.create(:deck_card, :with_persisted_card) }
-    it { is_expected.to validate_uniqueness_of(:card_id).scoped_to(:deck_id) }
+    it { is_expected.to validate_uniqueness_of(:card_id).scoped_to(:deck_id, :mainboard) }
   end
 end
